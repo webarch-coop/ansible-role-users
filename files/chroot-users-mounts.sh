aad335dcad9a725cd7fffa72083ed4e28bd330f4
Chris Croome <chris@webarchitects.co.uk>
2019-09-21 22:21:23 +0100

    chroot users mounts updated

--- files/chroot-users-mounts.sh
+++ files/chroot-users-mounts.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-chroot_users_dir=/etc/fstab.d
-# runs all the shell scripts in chroot_users_dir 
-run-parts ${chroot_users_dir}
+# run all the shell scripts in fstab.d
+run-parts /etc/fstab.d
