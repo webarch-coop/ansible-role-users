5cf09ad76faf56c49514b166b17194d53a94b65e
Chris Croome <chris@webarchitects.co.uk>
2019-10-07 12:09:26 +0100

    create /run/mysqld if it doesn't exist

--- files/chroot-users-mounts.sh
+++ files/chroot-users-mounts.sh
@@ -7,6 +7,12 @@ fi
 
 # Restart rsyslog
 service rsyslog try-restart && service rsyslog restart
+
+# Create /run/mysqld if it doesn't exist
+if [[ ! -d "/run/mysqld" ]]; then
+  mkdir /run/mysqld
+  chown mysql:root /run/mysqld
+fi
  
 # Run all the shell scripts in fstab.d
 run-parts /etc/fstab.d
